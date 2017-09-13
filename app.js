2110db0d0f9562ead0306621e5e2c61ef4ba42d4
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-13 11:38:24 +0700

    day la commit 3

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.post('/payload', function (req, res) {
 	//verify repository.name == 'wackcoon-device' or repository.full_name = 'DanielEgan/wackcoon-device'
 //	console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
 
-//	console.log('pulling code from GitHub...');
+	console.log('pulling code from GitHub...');
 
 	// reset any changes that have been made locally
-//	exec('git -C \Users\Phuong\Desktop\wackcoon-hook-master\wackcoon-hook-master reset --hard', execCallback);
+	exec('git -C \Users\Phuong\Desktop\wackcoon-hook-master\wackcoon-hook-master reset --hard', execCallback);
 
 	// and ditch any files that have been added locally too
 //	exec('git -C \Users\Phuong\Desktop\wackcoon-hook-master\wackcoon-hook-master clean -df', execCallback);
@@ -33,7 +33,7 @@ app.post('/payload', function (req, res) {
 	exec('git -C \Users\Phuong\Desktop\wackcoon-hook-master\wackcoon-hook-master pull -f', execCallback);
 
 	// and npm install with --production
-//	exec('npm -C \Users\Phuong\Desktop\wackcoon-hook-master\wackcoon-hook-master install --production', execCallback);
+	exec('npm -C \Users\Phuong\Desktop\wackcoon-hook-master\wackcoon-hook-master start', execCallback);
 
 	// and run tsc
 //	exec('tsc', execCallback);
